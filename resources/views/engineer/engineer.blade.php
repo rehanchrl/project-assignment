bef82e01d153967abe8ba96dce84fc9fabfb8133
rehanchrl <rehanchoirul@gmail.com>
2018-02-25 16:41:27 +0700

    added page admin,pm,engineer blade

---
+++ resources/views/engineer/engineer.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.admin_template')
+
+@section('content')
+<div class='row'>
+    <div class='col-md-6'>
+        <!-- Box -->
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">{{ Auth::user()->role }}</h3>
+                <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
+                </div>
+            </div>
+            <div class="box-body">
+                {{ Auth::user()->name }}
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+    </div><!-- /.col -->
+
+</div><!-- /.row -->
+@endsection
